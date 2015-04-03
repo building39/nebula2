56f060754111f1877f528b73ee02667912896e3e
mmartin <mmartin@mezeo.com>
2015-04-03 08:11:51 -0500

    Increase delay from 3 seconds to 5.

--- scripts/setup_riak_indexing.sh
+++ scripts/setup_riak_indexing.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-pushd $NEBULA_PATH/scripts
+pushd $NEBULA2_PATH/scripts
 
 RIAK_HOST=nebriak1
 RIAK_HTTP_PORT=8098
+DELAY=5
 
 # create cdmi bucket type
 ssh root@${RIAK_HOST} "riak-admin bucket-type create cdmi '{\"props\":{}}'"
 ssh root@${RIAK_HOST} "riak-admin bucket-type activate cdmi"
 
-sleep 3
+sleep ${DELAY}
 
 # register the schema with riak
 curl -v -XPUT "http://${RIAK_HOST}:${RIAK_HTTP_PORT}/search/schema/cdmi" -H 'Content-Type:application/xml' --data-binary @../config/cdmi_schema.xml
 
-sleep 3
+sleep ${DELAY}
 
 # create the index
 curl -v -XPUT "http://${RIAK_HOST}:${RIAK_HTTP_PORT}/search/index/cdmi_idx" -H 'Content-Type: application/json' -d '{"schema": "cdmi"}'
 
-sleep 3
+sleep ${DELAY}
 
 # apply index to bucket type cdmi
 curl -v -XPUT "http://${RIAK_HOST}:${RIAK_HTTP_PORT}/types/cdmi/buckets/cdmi/props" -H "Content-Type: application/json" -d '{"props": {"search_index": "cdmi_idx"}}'
