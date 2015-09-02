258eb277cc93179db95a5926d9f92733a4b452d9
mmartin <mmartin@mezeo.com>
2015-09-02 13:06:55 -0500

    New helper scripts.

---
+++ scripts/new_domain.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -v -uadministrator -H "x-cdmi-specification-version: 1.1" -H "content-type: application/cdmi-domain" -H "Accept: application/cdmi-domain" -X PUT "http://localhost:8080/cdmi/cdmi_domains/Fuzzcat" -d '{"metadata": {"cdmi_domain_enabled": "true"}}'
