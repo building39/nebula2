73633141c727530d125a29e3e403b466ecdf3331
mmartin <mmartin@mezeo.com>
2015-11-10 10:37:22 -0600

    Unit test for new_container/2

--- src/nebula2_containers.erl
+++ src/nebula2_containers.erl
@@ -5,6 +5,7 @@
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
+-include("nebula2_test.hrl").
 -endif.
 
 -include("nebula.hrl").
@@ -18,15 +19,12 @@
         ]).
 
 %% @doc Create a new CDMI container
--spec new_container(cowboy_req:req(), cdmi_state()) -> {boolean(), Req, cdmi_state()}
-        when Req::cowboy_req:req().
-new_container(Req, State) ->
+-spec new_container(cowboy_req:req(), cdmi_state()) -> {boolean(), cowboy_req:req(), cdmi_state()}.
+new_container(Req, State) when is_tuple(State) ->
     ?nebMsg("Entry"),
-    {_, EnvMap} = State,
-    Domain = nebula2_utils:get_value(<<"domainURI">>, EnvMap),
-    lager:debug("Domain: ~p", [Domain]),
     ObjectType = ?CONTENT_TYPE_CDMI_CONTAINER,
     {ok, Body, Req2} = cowboy_req:body(Req),
+    ?nebFmt("Body: ~p", [Body]),
     Body2 = try jsx:decode(Body, [return_maps]) of
                 NewBody ->
                     NewBody
@@ -34,18 +32,17 @@ new_container(Req, State) ->
                 error:badarg ->
                     throw(badjson)
             end,
-    Response = case nebula2_utils:create_object(State, ObjectType, Body2) of
-                   {true, Data} ->
-                       Data2 = nebula2_db:unmarshall(Data),
-                       {true, cowboy_req:set_resp_body(jsx:encode(maps:to_list(Data2)), Req2), State};
-                   false ->
-                       {false, Req2, State}
-               end,
-    Response.
+    case nebula2_utils:create_object(State, ObjectType, Body2) of
+        {true, Data} ->
+            Data2 = nebula2_db:unmarshall(Data),
+            {true, cowboy_req:set_resp_body(jsx:encode(maps:to_list(Data2)), Req2), State};
+        false ->
+            {false, Req2, State}
+    end.
 
 %% @doc Update a CDMI container
 -spec update_container(cowboy_req:req(), map(), object_oid()) -> {ok, json_value()}.
-update_container(Req, State, Oid) when is_map(State); is_binary(Oid)->
+update_container(Req, State, Oid) when is_tuple(State), is_binary(Oid)->
     ?nebMsg("Entry"),
     {Pid, _} = State,
     {ok, Body, Req2} = cowboy_req:body(Req),
@@ -66,15 +63,15 @@ update_container(Req, State, Oid) when is_map(State); is_binary(Oid)->
              <<"cdmi_mtime">>,
              <<"cdmi_acount">>,
              <<"cdmi_mcount">>],
+    ?nebMsg("About to update metadata"),
     Data3 = nebula2_utils:update_data_system_metadata(CList, Data2, State),
     ?nebFmt("Data3: ~p", [Data3]),
-    Response = case nebula2_db:update(Pid, Oid, nebula2_db:marshall(Data3)) of
-                   ok ->
-                       {true, Req2, State};
-                   _  ->
-                       {false, Req, State}
-               end,
-    Response.
+    case nebula2_db:update(Pid, Oid, nebula2_db:marshall(Data3)) of
+        ok ->
+            {true, Req2, State};
+        _  ->
+            {false, Req, State}
+    end.
 
 %% ====================================================================
 %% Internal functions
@@ -84,4 +81,70 @@ update_container(Req, State, Oid) when is_map(State); is_binary(Oid)->
 %% eunit tests
 %% ====================================================================
 -ifdef(EUNIT).
+nebula2_containers_test_() ->
+    {foreach,
+     fun() ->
+             meck:new(cowboy_req, [non_strict]),
+             meck:new(nebula2_utils, [passthrough])
+     end,
+     fun(_) ->
+             meck:unload(cowboy_req),
+             meck:unload(nebula2_utils)
+     end,
+     [{"new_container/2",
+       fun() ->
+            Pid = self(),
+            Req = "",
+            EnvMap = maps:from_list([{<<"path">>, <<"/new_container/">>},
+                                     {<<"auth_as">>, <<"MickeyMouse">>},
+                                     {<<"domainURI">>, <<"/cdmi_domains/system_domain/">>}
+                                    ]),
+            TestMap = jsx:decode(?TestSystemConfiguration, [return_maps]),
+            TestMapCDMI = maps:from_list([{<<"cdmi">>, TestMap},
+                                              {<<"sp">>, ?TestCreateContainerSearchPath}
+                                             ]),
+            State = {Pid, EnvMap},
+            meck:sequence(cowboy_req, body, 1, [{ok, <<"{}">>, Req}, {ok, "bad json", Req}]),
+            meck:sequence(cowboy_req, set_resp_body, 2, [Req]),
+            meck:sequence(nebula2_utils, create_object, 3, [{true, TestMapCDMI}]),
+            ?assertMatch({true, Req, State}, new_container(Req, State)),
+            ?assertException(throw, badjson, new_container(Req, State)),
+            ?assertException(error, function_clause, new_container(Req, not_a_tuple)),
+            ?assert(meck:validate(cowboy_req)),
+            ?assert(meck:validate(nebula2_utils))
+       end
+%%       },
+%%       {"Test update_container/3",
+%%        fun() ->
+%%             Pid = self(),
+%% 			Req = "",
+%% 			EnvMap = maps:from_list([{<<"path">>, <<"/new_container/">>},
+%%                                      {<<"auth_as">>, <<"MickeyMouse">>},
+%%                                      {<<"domainURI">>, <<"/cdmi_domains/system_domain/">>}
+%%                                     ]),
+%% 			TestMap = jsx:decode(?TestSystemConfiguration, [return_maps]),
+%% 			TestMapCDMI = maps:from_list([{<<"cdmi">>, TestMap},
+%%                                               {<<"sp">>, ?TestCreateContainerSearchPath}
+%%                                              ]),
+%%             State = {Pid, EnvMap},
+%% 			Oid = maps:get(<<"objectID">>, TestMap),
+%% 			Body = <<"{\"metadata\": {\"new_metadata\": \"junk\"}}">>,
+%% 			Capabilities = jsx:decode(?TestSystemCapabilities, [return_maps]),
+%% 			meck:reset(cowboy_req),
+%% 			meck:reset(nebula2_db),
+%%             meck:sequence(cowboy_req, body, 1, [{ok, Body, Req}, {ok, "bad json", Req}]),
+%% 			meck:sequence(nebula2_db, read, 2, [{ok, TestMapCDMI}]),
+%% 			meck:sequence(nebula2_db, search, 2, [{ok, Capabilities}]),
+%% 			meck:sequence(nebula2_db, update, 3, [{true, Req, State}, {false, Req, State}]),
+%% 			?assertMatch({true, Req, State}, update_container(Req, State, Oid)),
+%% 			?assertMatch({false, Req, State}, update_container(Req, State, Oid)),
+%% 			?assertException(throw, badjson, update_container(Req, State, Oid)),
+%%             ?assertException(error, function_clause, update_container(Req, not_a_tuple, Oid)),
+%% 			?assertException(error, function_clause, update_container(Req, State, not_a_binary)),
+%% 			?assert(meck:validate(cowboy_req)),
+%% 			?assert(meck:validate(nebula2_db))
+%%         end
+      }
+	]
+  }.
 -endif.
