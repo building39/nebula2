dada2306d15d6d165af79367f409ecb3ff64bb51
mmartin <mmartin@mezeo.com>
2016-09-06 17:08:51 -0500

    Add Script.sql

---
+++ Script.sql
@@ -0,0 +1,21 @@
+--<ScriptOptions statementTerminator="GO"/>
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE VIEW dbo.InternCertificationTypes
+AS
+SELECT DISTINCT InternID, CertificationTypeID, InternCertificationID AS id
+FROM            dbo.InternCertification
+WHERE        (CertificationTypeID NOT IN (1, 22))
+GO
+
+SET ANSI_PADDING OFF
+GO
+
